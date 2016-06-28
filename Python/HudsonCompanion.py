2f2e5388b53dd1c58b3fddc9782897f5cf299952
kasemir <kasemirk@ornl.gov>
2016-06-28 15:33:25 -0400

    Check in loop

--- Python/HudsonCompanion.py
+++ Python/HudsonCompanion.py
@@ -10,18 +10,23 @@ else:
         url = sys.argv[2]
     else:
         url = "http://ics-web4.sns.ornl.gov:8185/api/xml"
-    stats = HudsonChecker(url).jobStats()
-    stats = str(int(100*stats[0]/stats[1]))
-    data = stats.encode('utf-8')
-    print(stats)
-    print(data)
 
-    ser = serial.Serial(port=sys.argv[1], timeout=5)
+    ser = serial.Serial(sys.argv[1], 9600, timeout=5, xonxoff=0, rtscts=0)
+    # Wait for Arduino to boot up after opening the serial line reset it
+    time.sleep(5)
     readout = ser.read(1) #wait until signal sent to write
-    ser.write(data)
-    while readout and readout != '\n' and readout != '\r':
-        print(readout.decode('utf-8'), end="", flush=True)
-        readout = ser.read()
-    print("^C to exit")
-    while True: #endless loop, prevents Serial close and reset
-        time.sleep(1)
+
+    while True:
+        print("Checking Hudson status")
+        stats = HudsonChecker(url).jobStats()
+        stats = str(int(100*stats[0]/stats[1])) + "\n"
+        data = stats.encode('utf-8')
+        print(stats)
+        print(data)
+    
+        ser.write(data)
+        while readout and readout != '\n' and readout != '\r':
+            print(readout.decode('utf-8'), end="", flush=True)
+            readout = ser.read()
+        print("^C to exit")
+        time.sleep(10)
